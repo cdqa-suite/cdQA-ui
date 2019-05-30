129f1c1a7cf671bb3d9a48cf6662684b2394b245
fmikaelian <39884124+fmikaelian@users.noreply.github.com>
2019-05-30 16:05:26 +0200

    update deps + add FA icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import BootstrapVue from 'bootstrap-vue'
+import ToggleButton from 'vue-js-toggle-button'
 
 Vue.use(BootstrapVue);
+Vue.use(ToggleButton);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
